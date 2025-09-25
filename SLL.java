7cd2370679d42f018b206d3bea8b7a64960db0ae
Caitlyn Kim <cakim@smith.edu>
2025-09-25 12:05:52 -0400

    getting started on SLL.java

--- SLL.java
+++ SLL.java
@@ -1,10 +1,46 @@
 /**
  * Class to implement a singly linked list
  *
- * @author 
- * @version Spring 2024
+ * @author Caitlyn Kim
+ * @version Fall 2025
  */
 
- public class SLL<T> {
+ public class SLL<T> implements Phase1SLL<T> {
+  /** 
+   *  Accessor for head node
+   *  @return the head node
+   */
+  public NodeSL<T> getHead(){
+    return node;
+  }
   
+  /** 
+   *  Accessor for tail node
+   *  @return the tail node
+   */
+  public NodeSL<T> getTail(){
+
+  }
+
+  /** 
+   *  Determines whether a list is empty
+   *  @return T/F is the list empty?
+   */
+  public boolean isEmpty(){
+    return true;
+  }
+
+  /** 
+   *  Inserts the given item at the head of the list
+   *  @param v item to insert 
+   */
+  public void addFirst(T v){
+
+  }
+  
+  /** Converts to a string representation */
+  public String toString(){
+    return "hi";
+  }
+
  }
